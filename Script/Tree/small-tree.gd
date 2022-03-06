375a4c763718a29d087e25a25451e8d0f6c0c9f0
Viet281101 <nva281101@gmail.com>
2022-03-06 19:36:02 +0100

    project

--- Script/Tree/small-tree.gd
+++ Script/Tree/small-tree.gd
@@ -4,7 +4,7 @@ extends Node2D
 const Drop = preload("res://Scene/Collect_EXP.tscn")
 var rng = RandomNumberGenerator.new()
 var my_number
-onready var hurtBox = $HurtBox2
+export (NodePath) onready var hurtBox = get_node(hurtBox) as Area2D
 
 func create_tree_effect():
 	var small_tree_effect = load("res://Scene/Tree/smal-tree-effect.tscn")
@@ -23,7 +23,6 @@ func _on_HurtBox_area_entered(_area):
 	create_tree_effect()
 	queue_free()
 
-
 func _on_HurtBox_area_exited(_area):
 	rng.randomize()
 	my_number = rng.randi_range(0,20)
@@ -36,4 +35,15 @@ func _on_HurtBox_area_exited(_area):
 
 func _on_HurtBox2_area_entered(_area):
 	hurtBox.fire_hurt_effect(0.04, 0.8)
-	hurtBox.start_invincibility(0.4)
+	hurtBox.start_invincibility(0.6)
+	rng.randomize()
+	my_number = rng.randi_range(0,80)
+	if my_number <= 5:
+		create_tree_effect()
+	elif my_number >= 75:
+		var world = get_tree().current_scene
+		var drop = Drop.instance()
+		world.call_deferred("add_child", drop)
+		drop.position = position
+		queue_free()
+
